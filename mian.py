ace20df5d097c61faf683f9a792ef492b924c18a
Naitik Dhiman <naitikdhiman.dev@gmail.com>
2026-06-14 15:28:34 +0530

    Create mian.py

---
+++ mian.py
@@ -0,0 +1,13 @@
+from monitor.file_monitor import start_monitoring
+from honeypot.honeyfile_generator import create_honeyfiles
+
+def main():
+print("Starting AI-Based Ransomware Detection System...")
+
+```
+create_honeyfiles()
+start_monitoring()
+```
+
+if **name** == "**main**":
+main()
